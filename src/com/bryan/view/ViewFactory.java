b3b1d3f6c3513f3140b79ada56df89e4507944d8
Bryan <bryan.dov.bergman@gmail.com>
2020-07-27 21:27:55 +0300

    Adding files from tabletPC

--- src/com/bryan/view/ViewFactory.java
+++ src/com/bryan/view/ViewFactory.java
@@ -1,6 +1,15 @@
 package com.bryan.view;
 
 import com.bryan.EmailManager;
+import com.bryan.controller.BaseController;
+import com.bryan.controller.LoginWindowController;
+import com.bryan.controller.MainWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
 
@@ -11,7 +20,34 @@ public class ViewFactory {
     }
 
     public void showLoginWindow(){
-        System.out.println("Show login window");
+        System.out.println("Show login window called");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        initializeStage(controller);
+
     }
 
+    public void showMainWindow(){
+        System.out.println("Show main window called");
+
+        BaseController controller = new MainWindowController(emailManager, this, "MainWindow.fxml");
+        initializeStage(controller);
+    }
+
+    private void initializeStage(BaseController baseController){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(baseController.getFxmlName()));
+        fxmlLoader.setController(baseController);
+        Parent parent;
+        try{
+            parent = fxmlLoader.load();
+        }catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
